c9c620802759dd82e1dcad173b3c0d173d1d5c17
Alexander Urizar <urizaraj@gmail.com>
2018-03-14 14:41:01 +0000

    automatically backed up by learn

--- db/migrate/20180314143905_d_ids_to_rides.rb
+++ db/migrate/20180314143905_d_ids_to_rides.rb
@@ -1,4 +1,4 @@
-class AddIdsToRides < ActiveRecord::Migration
+class DIdsToRides < ActiveRecord::Migration
   def change
     add_column :rides, :taxi_id, :integer
     add_column :rides, :passenger_id, :integer
